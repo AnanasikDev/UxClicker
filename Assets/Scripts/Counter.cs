84beb6972271035ec111113dc0dc906d68689c9f
Олег Бровченков <oleg.brovchenkov@gmail.com>
2022-01-30 14:16:42 +0300

    UX update

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -10,7 +10,10 @@ public class Counter : MonoBehaviour
     [SerializeField] private ParticleSystem ParticleSystem;
     [SerializeField] private ParticleSystem ParticleSystem10;
     [SerializeField] private ParticleSystem ParticleSystem25;
+    [SerializeField] private ParticleSystem ParticleSystem50;
     [SerializeField] private ParticleSystem ParticleSystem100;
+    [SerializeField] private ParticleSystem ParticleSystem250;
+    [SerializeField] private ParticleSystem ParticleSystem500;
     [SerializeField] private ParticleSystem ParticleSystem1000;
     [SerializeField] private ParticleSystem[] PresentParticleSystems;
 
@@ -25,27 +28,48 @@ public class Counter : MonoBehaviour
         Clicks++;
         ClickTextMeshPro.text = Clicks.ToString();
         _ButtonAnimator.Rebind();
-        _ButtonAnimator.SetBool("Click", true);
+        _ButtonAnimator.SetTrigger("Click");
         _TextAnimator.Rebind();
-        _TextAnimator.SetBool("Click" + Random.Range(1, 2+1), true);
 
-        if (Clicks % 10 == 0)
-            ParticleSystem10.Play();
-        if (Clicks % 25 == 0)
-            ParticleSystem25.Play();
-        if (Clicks % 100 == 0)
+        if (Clicks % 1000 == 0)
+        {
+            Debug.Log("S");
+            ParticleSystem1000.Play();
+            PlayPresentsEffect();
+            _TextAnimator.SetTrigger("Click1000");
+        }
+        else if (Clicks % 500 == 0)
+        {
+            ParticleSystem500.Play();
+            _TextAnimator.SetTrigger("Click500");
+        }
+        else if (Clicks % 250 == 0)
+        {
+            ParticleSystem250.Play();
+            _TextAnimator.SetTrigger("Click250");
+        }
+        else if (Clicks % 100 == 0)
         {
             ParticleSystem100.Play();
             PlayPresentsEffect();
-            _TextAnimator.SetBool("Click100" + Random.Range(1, 2 + 1), true);
+            _TextAnimator.SetTrigger("Click100");
         }
-        if (Clicks % 1000 == 0)
+
+        if (Clicks % 50 == 0)
+            ParticleSystem50.Play();
+        if (Clicks % 25 == 0)
+            ParticleSystem25.Play();
+        if (Clicks % 10 == 0)
+            ParticleSystem10.Play();
+
+        else
         {
-            ParticleSystem1000.Play();
-            PlayPresentsEffect();
-            _TextAnimator.SetBool("Click1000" + Random.Range(1, 2 + 1), true);
+            var particleSystem = Instantiate(ParticleSystem);
+            particleSystem.Play();
+            Destroy(particleSystem.gameObject, 6);
+            _TextAnimator.SetTrigger("Click" + Random.Range(1, 2 + 1));
         }
-        else ParticleSystem.Play();
+        
         SaveScore();
     }
     private void PlayPresentsEffect()
